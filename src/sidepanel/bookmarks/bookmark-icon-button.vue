f1daa18f8c459f3fd09d4bb760268bb01cae3344
Zeta <zetaztt@outlook.com>
2026-07-01 15:35:48 +0800

    抽象书签图标按钮并统一悬浮样式

---
+++ src/sidepanel/bookmarks/bookmark-icon-button.vue
@@ -0,0 +1,71 @@
+<script lang="ts" setup>
+defineProps<{
+	icon: string;
+	title: string;
+	disabled?: boolean;
+}>();
+
+const emit = defineEmits<{
+	click: [];
+}>();
+</script>
+
+<template>
+	<button
+		class="bookmark-icon-button"
+		type="button"
+		:disabled="disabled"
+		:title="title"
+		:aria-label="title"
+		@click.stop="emit('click')">
+		<span class="bookmark-icon-button-icon" :style="{ backgroundImage: `url(${icon})` }"></span>
+	</button>
+</template>
+
+<style scoped>
+.bookmark-icon-button {
+	position: relative;
+	display: inline-block;
+	width: 39px;
+	height: 30px;
+	margin-bottom: 0;
+	border: 0;
+	border-color: transparent;
+	border-radius: 0;
+	padding: 0;
+	color: #e2e2e2;
+	background: transparent;
+	background-image: none;
+	font: inherit;
+	font-size: 13px;
+	font-weight: 400;
+	line-height: 20px;
+	text-align: center;
+	vertical-align: middle;
+	white-space: nowrap;
+	cursor: pointer;
+	user-select: none;
+}
+
+.bookmark-icon-button:hover:not(:disabled) {
+	background-color: #2d3136;
+}
+
+.bookmark-icon-button:disabled {
+	opacity: 0.6;
+	cursor: default;
+}
+
+.bookmark-icon-button-icon {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	width: 20px;
+	height: 20px;
+	margin-top: -10px;
+	margin-left: -10px;
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: contain;
+}
+</style>
